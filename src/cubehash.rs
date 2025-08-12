7ddee3451dec659de23df6f956f63ed4517a246d
Matthieu Crepeau <matthieu@rimba-net.com>
2025-08-12 13:40:11 -0500

    Added ARM NEON support

--- src/cubehash.rs
+++ src/cubehash.rs
@@ -24,6 +24,13 @@ use core::arch::aarch64::{
     uint32x4_t, vaddq_u32, vdupq_n_u32, vextq_u32, vld1q_u32, vrev64q_u32, vshlq_n_u32, vshrq_n_u32,
     veorq_u32, vst1q_u32,
 };
+#[cfg(target_arch = "arm")]
+use core::arch::arm::{
+    uint32x4_t as uint32x4_t_arm, vaddq_u32 as vaddq_u32_arm, vdupq_n_u32 as vdupq_n_u32_arm,
+    vextq_u32 as vextq_u32_arm, vld1q_u32 as vld1q_u32_arm, vrev64q_u32 as vrev64q_u32_arm,
+    vshlq_n_u32 as vshlq_n_u32_arm, vshrq_n_u32 as vshrq_n_u32_arm, veorq_u32 as veorq_u32_arm,
+    vst1q_u32 as vst1q_u32_arm,
+};
 #[cfg(not(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64")))]
 use crate::u32x4::{add, xor, U32x4};
 
@@ -454,6 +461,141 @@ unsafe fn _cubehash_avx2<R: Read>(input: &mut R, irounds: i32, frounds: i32, has
     [x0b, x1b, x2b, x3b].concat()
 }
 
+// ARMv7 (arm32) NEON backend with runtime detection
+#[cfg(all(target_arch = "arm", target_feature = "neon"))]
+#[target_feature(enable = "neon")]
+unsafe fn _cubehash_neon32<R: Read>(input: &mut R, irounds: i32, frounds: i32, hashlen: i32) -> Vec<u8> {
+    let mut done = false;
+    let mut eof = false;
+    let mut more = true;
+    let mut data: [u8; BUFSIZE as usize] = [0; BUFSIZE as usize];
+
+    let mut x0: uint32x4_t_arm = vld1q_u32_arm([0u32, ROUNDS as u32, BLOCKSIZE as u32, (hashlen / 8) as u32].as_ptr());
+    let mut x1: uint32x4_t_arm = vdupq_n_u32_arm(0);
+    let mut x2: uint32x4_t_arm = vdupq_n_u32_arm(0);
+    let mut x3: uint32x4_t_arm = vdupq_n_u32_arm(0);
+    let mut x4: uint32x4_t_arm = vdupq_n_u32_arm(0);
+    let mut x5: uint32x4_t_arm = vdupq_n_u32_arm(0);
+    let mut x6: uint32x4_t_arm = vdupq_n_u32_arm(0);
+    let mut x7: uint32x4_t_arm = vdupq_n_u32_arm(0);
+
+    let mut y0: uint32x4_t_arm;
+    let mut y1: uint32x4_t_arm;
+    let mut y2: uint32x4_t_arm;
+    let mut y3: uint32x4_t_arm;
+
+    let mut datasize = irounds / ROUNDS * BLOCKSIZE;
+
+    #[inline(always)]
+    unsafe fn load_le_u32x4_reversed_arm(bytes: &[u8]) -> uint32x4_t_arm {
+        let a = u32::from_le_bytes([bytes[12], bytes[13], bytes[14], bytes[15]]);
+        let b = u32::from_le_bytes([bytes[8], bytes[9], bytes[10], bytes[11]]);
+        let c = u32::from_le_bytes([bytes[4], bytes[5], bytes[6], bytes[7]]);
+        let d = u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]);
+        vld1q_u32_arm([a, b, c, d].as_ptr())
+    }
+
+    #[inline(always)]
+    unsafe fn to_bytes_d_c_b_a_arm(v: uint32x4_t_arm) -> [u8; 16] {
+        let mut arr = [0u32; 4];
+        vst1q_u32_arm(arr.as_mut_ptr(), v);
+        let bytes = [
+            arr[3].to_le_bytes(),
+            arr[2].to_le_bytes(),
+            arr[1].to_le_bytes(),
+            arr[0].to_le_bytes(),
+        ];
+        [
+            bytes[0][0], bytes[0][1], bytes[0][2], bytes[0][3],
+            bytes[1][0], bytes[1][1], bytes[1][2], bytes[1][3],
+            bytes[2][0], bytes[2][1], bytes[2][2], bytes[2][3],
+            bytes[3][0], bytes[3][1], bytes[3][2], bytes[3][3],
+        ]
+    }
+
+    #[inline(always)]
+    unsafe fn permute_badc_arm(v: uint32x4_t_arm) -> uint32x4_t_arm { vrev64q_u32_arm(v) }
+    #[inline(always)]
+    unsafe fn permute_cdab_arm(v: uint32x4_t_arm) -> uint32x4_t_arm { vextq_u32_arm::<2>(v, v) }
+
+    while !done {
+        let mut pos: i32 = 0;
+        let end: i32 = datasize - 1;
+
+        while pos < end {
+            x0 = veorq_u32_arm(x0, load_le_u32x4_reversed_arm(&data[pos as usize..(pos + 16) as usize]));
+            pos += 16;
+
+            x1 = veorq_u32_arm(x1, load_le_u32x4_reversed_arm(&data[pos as usize..(pos + 16) as usize]));
+            pos += 16;
+            
+            for _i in 0..ROUNDS {
+                x4 = vaddq_u32_arm(x0, permute_badc_arm(x4));
+                x5 = vaddq_u32_arm(x1, permute_badc_arm(x5));
+                x6 = vaddq_u32_arm(x2, permute_badc_arm(x6));
+                x7 = vaddq_u32_arm(x3, permute_badc_arm(x7));
+                y0 = x2;
+                y1 = x3;
+                y2 = x0;
+                y3 = x1;
+                x0 = veorq_u32_arm(vshlq_n_u32_arm::<7>(y0), vshrq_n_u32_arm::<25>(y0));
+                x1 = veorq_u32_arm(vshlq_n_u32_arm::<7>(y1), vshrq_n_u32_arm::<25>(y1));
+                x2 = veorq_u32_arm(vshlq_n_u32_arm::<7>(y2), vshrq_n_u32_arm::<25>(y2));
+                x3 = veorq_u32_arm(vshlq_n_u32_arm::<7>(y3), vshrq_n_u32_arm::<25>(y3));
+                x0 = veorq_u32_arm(x0, x4);
+                x1 = veorq_u32_arm(x1, x5);
+                x2 = veorq_u32_arm(x2, x6);
+                x3 = veorq_u32_arm(x3, x7);
+
+                x4 = vaddq_u32_arm(x0, permute_cdab_arm(x4));
+                x5 = vaddq_u32_arm(x1, permute_cdab_arm(x5));
+                x6 = vaddq_u32_arm(x2, permute_cdab_arm(x6));
+                x7 = vaddq_u32_arm(x3, permute_cdab_arm(x7));
+                y0 = x1;
+                y1 = x0;
+                y2 = x3;
+                y3 = x2;
+                x0 = veorq_u32_arm(vshlq_n_u32_arm::<11>(y0), vshrq_n_u32_arm::<21>(y0));
+                x1 = veorq_u32_arm(vshlq_n_u32_arm::<11>(y1), vshrq_n_u32_arm::<21>(y1));
+                x2 = veorq_u32_arm(vshlq_n_u32_arm::<11>(y2), vshrq_n_u32_arm::<21>(y2));
+                x3 = veorq_u32_arm(vshlq_n_u32_arm::<11>(y3), vshrq_n_u32_arm::<21>(y3));
+                x0 = veorq_u32_arm(x0, x4);
+                x1 = veorq_u32_arm(x1, x5);
+                x2 = veorq_u32_arm(x2, x6);
+                x3 = veorq_u32_arm(x3, x7);
+            }
+        }
+        done = !more;
+
+        if more {
+            if eof {
+                datasize = frounds / ROUNDS * BLOCKSIZE;
+                for i in &mut data[0..datasize as usize] { *i = 0 }
+                x7 = veorq_u32_arm(x7, vld1q_u32_arm([0u32, 1, 0, 0].as_ptr()));
+                more = false;
+            } else {
+                datasize = input.read(&mut data).unwrap() as i32;
+                if datasize < BUFSIZE {
+                    let padsize = BLOCKSIZE - datasize % BLOCKSIZE;
+                    for i in &mut data[datasize as usize..(datasize + padsize) as usize] { *i = 0 }
+                    data[datasize as usize] = 0x80;
+                    datasize += padsize;
+                    eof = true;
+                }
+            }
+        }
+    }
+
+    let out = [
+        to_bytes_d_c_b_a_arm(x0),
+        to_bytes_d_c_b_a_arm(x1),
+        to_bytes_d_c_b_a_arm(x2),
+        to_bytes_d_c_b_a_arm(x3),
+    ]
+    .concat();
+    out
+}
+
 pub fn cubehash<R: Read>(input: &mut R, revision: i32, hashlen: i32) -> Vec<u8> {
     unsafe {
         match if hashlen <= MAXHASHLEN && hashlen % 8 == 0 { revision }  else  { 0 } {
@@ -464,6 +606,12 @@ pub fn cubehash<R: Read>(input: &mut R, revision: i32, hashlen: i32) -> Vec<u8> 
                         return _cubehash_avx2(input, 16, 32, hashlen);
                     }
                 }
+                #[cfg(target_arch = "arm")]
+                {
+                    if std::arch::is_arm_feature_detected!("neon") {
+                        return _cubehash_neon32(input, 16, 32, hashlen);
+                    }
+                }
                 return _cubehash(input, 16, 32, hashlen);
             },
             2 => {
@@ -471,6 +619,12 @@ pub fn cubehash<R: Read>(input: &mut R, revision: i32, hashlen: i32) -> Vec<u8> 
                 {
                     if std::arch::is_x86_feature_detected!("avx2") {
                         return _cubehash_avx2(input, 160, 160, hashlen);
+                    }
+                }
+                #[cfg(target_arch = "arm")]
+                {
+                    if std::arch::is_arm_feature_detected!("neon") {
+                        return _cubehash_neon32(input, 160, 160, hashlen);
                     }
                 }
                 return _cubehash(input, 160, 160, hashlen);
